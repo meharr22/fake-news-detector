b36bbb6bdb33c0075398b4ba2f9c7d86d35821bf
Mehar Arora <mmehar_be23@thapar.edu>
2026-04-02 10:13:58 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,14 @@
 import pickle
+import os
 from src.preprocess import clean_text
 
-model = pickle.load(open("models/model.pkl", "rb"))
-vectorizer = pickle.load(open("models/vectorizer.pkl", "rb"))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+model_path = os.path.join(BASE_DIR, "models", "model.pkl")
+vectorizer_path = os.path.join(BASE_DIR, "models", "vectorizer.pkl")
+
+model = pickle.load(open(model_path, "rb"))
+vectorizer = pickle.load(open(vectorizer_path, "rb"))
 
 def predict_news(text):
     cleaned = clean_text(text)
